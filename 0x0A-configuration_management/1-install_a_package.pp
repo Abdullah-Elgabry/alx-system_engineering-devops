5c54f362d921654de99c39659ac9eb3d56d3aa87
“abdullah <abdullah.h.elgabry@gmail.com>
2024-04-19 22:32:09 +0200

    config management task #done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# this script for make install flask from pip.
+# this script for make install flask from pip
 
 package { 'puppet-lint':
-  ensure   => '2.5.0',
+  ensure   => '2.1.0',
   provider => 'gem',
 }
