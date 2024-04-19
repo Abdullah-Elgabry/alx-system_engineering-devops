c8cd5a8bae45992f635e3bc473f5ddf8bf7a7551
“abdullah <abdullah.h.elgabry@gmail.com>
2024-04-19 22:45:47 +0200

    config management task #done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # this will nstall flask - Version must be 2.1.0
 
+package { 'Werkzeug':
+ensure   => '2.1.1',
+provider => 'pip3',
+}
+
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+  ensure => '2.1.0',
+provider => 'pip3',
 }
