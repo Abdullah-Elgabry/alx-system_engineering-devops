3b92a6c562c750e0a2a804cb05ce3fc26dff672b
“abdullah <abdullah.h.elgabry@gmail.com>
2024-06-05 00:40:10 +0300

    adv ver 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,6 @@ import requests
 
 def number_of_subscribers(subreddit):
     """function for getting the num of followers of reddit"""
-
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
