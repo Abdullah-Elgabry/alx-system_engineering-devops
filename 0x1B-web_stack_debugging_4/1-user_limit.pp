008210e90eb70bbe93d0b706fab5fa55d596990b
“abdullah <abdullah.h.elgabry@gmail.com>
2024-06-13 19:29:19 +0300

    0x1B

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+# config operatting sys.
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
